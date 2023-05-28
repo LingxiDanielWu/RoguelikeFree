55dc5350048d9675c783d479c9bed79ef4ed7aff
SherlockShen <18816298692@163.com>
2023-05-28 18:42:34 +0800

    还是用上Lua吧

---
+++ Assets/Scripts/XluaInitTest.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+public class XluaInitTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        LuaEnv luaenv = new LuaEnv();
+        luaenv.DoString("CS.UnityEngine.Debug.Log('hello world')");
+        luaenv.Dispose();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
